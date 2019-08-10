64c882db2cea07bd8aebb248f7d69a3145c3b6e0
Noah Fairbairn <noahfairbairn@gmail.com>
2019-08-10 12:56:54 -0700

    added create to posts

--- travel-api/app/controllers/posts_controller.rb
+++ travel-api/app/controllers/posts_controller.rb
@@ -4,4 +4,10 @@ class PostsController < ApplicationController
         posts = Post.all
         render json: posts, include: [:collection, :images]
     end
+
+    def create
+        # byebug
+        post = Post.create(title: params[:title], preview: params[:preview], content: params[:content], blog_id: params[:blog_id], location_id: params[:location_id])
+        render json: post
+    end
 end
